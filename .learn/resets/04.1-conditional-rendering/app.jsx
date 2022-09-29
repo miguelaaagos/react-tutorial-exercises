0724d168aad25d699c799d35f86d81831415a4b7
Miguel Palacios <migue.laagos@gmail.com>
2022-09-29 04:50:12 +0000

    Completado

--- .learn/resets/04.1-conditional-rendering/app.jsx
+++ .learn/resets/04.1-conditional-rendering/app.jsx
@@ -2,27 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
-const Alert = (props) => {
+const Alert = props => {
 	//your component here
-	let color = "alert-primary";
-	if (props.color == "red") color = "alert-danger";
-	else if (props.color == "orange") color = "alert-warning";
 
-	return (
-		<div className={`alert ${color}`} role="alert">
-			{props.text}
-		</div>
-	);
 };
-
-const colorClasses = {
-	red: "alert-danger",
-	orange: "alert-warning",
-};
-
 Alert.propTypes = {
 	color: PropTypes.string,
-	text: PropTypes.string,
+	text: PropTypes.string
 };
 
 // here is where the alert component is being used, you don't have to edit this part,
